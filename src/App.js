77c066c18abaec687173c1fc55969a0cc7bfa7af
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-05 11:24:24 -0400

    Add App scaffolding

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='App-left'>
+          
+        </div>
+        <div className='App-right'>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
