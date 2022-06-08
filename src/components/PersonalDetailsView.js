549c6581c01b94863595e69ce175b458e1afac73
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-07 21:42:44 -0400

    Add styling to Personal Details View and the cv entry form

--- src/components/PersonalDetailsView.js
+++ src/components/PersonalDetailsView.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import Icon from '@mdi/react';
 import { mdiPhone, mdiEmail, mdiMapMarker } from '@mdi/js';
+import '../styles/PersonalDetailsView.css';
 
 class PersonalDetailsView extends Component {
     render() {
         const { name, title, phone, email, location, summary } = this.props;
         return (
-            <div>
-                <div>
-                    <div>
-                        <p>{name}</p>
-                        <p>{title}</p>
+            <div className='personal-details-view'>
+                <div className='personal-details'>
+                    <div className='personal-details-left'>
+                        <p className='personal-name'>{name}</p>
+                        <p className='personal-title'>{title}</p>
                     </div>
-                    <div>
-                        <Icon path={mdiPhone} size={'12px'} />
-                        <p>{phone}</p>
-                    </div>
-                    <div>
-                        <Icon path={mdiEmail} size={'12px'} />
-                        <p>{email}</p>
-                    </div>
-                    <div>
-                        <Icon path={mdiMapMarker} size={'12px'} />
-                        <p>{location}</p>
-                    </div>
+                    <div className='personal-details-right'>
+                        <div className='personal-detail'>
+                            <Icon path={mdiPhone} size={'16px'} />
+                            <p>{phone}</p>
+                        </div>
+                        <div className='personal-detail'>
+                            <Icon path={mdiEmail} size={'16px'} />
+                            <p>{email}</p>
+                        </div>
+                        <div className='personal-detail'>
+                            <Icon path={mdiMapMarker} size={'16px'} />
+                            <p>{location}</p>
+                        </div>
+                    </div>    
                 </div>
                 <hr />
-                <p>{summary}</p>
+                <p className='personal-summary'>{summary}</p>
             </div>
         );
     }
