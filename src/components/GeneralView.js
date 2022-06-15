8129aa5df5dde4fcee7f1a41d76b94395dd558f6
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-15 19:11:20 -0400

    Convert GeneralView from class component to functional component

--- src/components/GeneralView.js
+++ src/components/GeneralView.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Icon from '@mdi/react';
 import { mdiPhone, mdiEmail, mdiMapMarker } from '@mdi/js';
 import '../styles/GeneralView.css';
 
-class GeneralView extends Component {
-    render() {
-        const { name, title, phone, email, location, summary } = this.props;
-        return (
-            <div className='GeneralView-view'>
-                <div className='GeneralView-details'>
-                    <div className='GeneralView-details-left'>
-                        <p className='GeneralView-name'>{name}</p>
-                        <p className='GeneralView-title'>{title}</p>
-                    </div>
-                    <div className='GeneralView-details-right'>
-                        <div className='GeneralView-detail'>
-                            <Icon path={mdiPhone} size={'16px'} />
-                            <p>{phone}</p>
-                        </div>
-                        <div className='GeneralView-detail'>
-                            <Icon path={mdiEmail} size={'16px'} />
-                            <p>{email}</p>
-                        </div>
-                        <div className='GeneralView-detail'>
-                            <Icon path={mdiMapMarker} size={'16px'} />
-                            <p>{location}</p>
-                        </div>
-                    </div>    
+const GeneralView = (props) => {
+    const { name, title, phone, email, location, summary } = props;
+    return (
+        <div className='GeneralView-view'>
+            <div className='GeneralView-details'>
+                <div className='GeneralView-details-left'>
+                    <p className='GeneralView-name'>{name}</p>
+                    <p className='GeneralView-title'>{title}</p>
                 </div>
-                <hr />
-                <p className='GeneralView-summary'>{summary}</p>
+                <div className='GeneralView-details-right'>
+                    <div className='GeneralView-detail'>
+                        <Icon path={mdiPhone} size={'16px'} />
+                        <p>{phone}</p>
+                    </div>
+                    <div className='GeneralView-detail'>
+                        <Icon path={mdiEmail} size={'16px'} />
+                        <p>{email}</p>
+                    </div>
+                    <div className='GeneralView-detail'>
+                        <Icon path={mdiMapMarker} size={'16px'} />
+                        <p>{location}</p>
+                    </div>
+                </div>
             </div>
-        );
-    }
-}
+            <hr />
+            <p className='GeneralView-summary'>{summary}</p>
+        </div>
+    );
+};
 
 export default GeneralView;
