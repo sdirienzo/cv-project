a3d119b25ed178ed5fa3339c598b74e52fdba41c
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-15 19:21:36 -0400

    Convert EducationView from class component to functional component

--- src/components/EducationView.js
+++ src/components/EducationView.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/EducationView.css';
 
-class EducationView extends Component {
-    render() {
-        const { school, field, start, end } = this.props;
-        return (
-            <div className='EducationView-view'>
-                <div className='EducationView-row'>
-                    <div className='EducationView-row-left'>
-                        <p className='EducationView-field'>{field}</p>
-                    </div>
-                    <div className='EducationView-row-right'>
-                        <p className='EducationView-school'>{school}</p>
-                        <span className='EducationView-divider'>|</span>
-                        <p className='EducationView-dates'>{start} - {end}</p>
-                    </div>
+const EducationView = (props) => {
+    const { school, field, start, end } = props;
+    return (
+        <div className='EducationView-view'>
+            <div className='EducationView-row'>
+                <div className='EducationView-row-left'>
+                    <p className='EducationView-field'>{field}</p>
+                </div>
+                <div className='EducationView-row-right'>
+                    <p className='EducationView-school'>{school}</p>
+                    <span className='EducationView-divider'>|</span>
+                    <p className='EducationView-dates'>{start} - {end}</p>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default EducationView;
