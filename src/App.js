1933e98afebac9676c5b9ae2f9e346ac9213f903
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-15 19:53:20 -0400

    Convert App from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import uniqid from 'uniqid';
 import GeneralView from './components/GeneralView';
@@ -8,74 +8,71 @@ import EducationView from './components/EducationView';
 import WorkForm from './components/WorkForm';
 import WorkView from './components/WorkView';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = (props) => {
 
-    this.state = {
-      general: {
-        name: '',
-        title: '',
-        phone: '',
-        email: '',
-        location: '',
-        summary: '',
+  const [general, setGeneral] = useState(
+    {
+      name: '',
+      title: '',
+      phone: '',
+      email: '',
+      location: '',
+      summary: '',
+    }
+  );
+
+  const [educationExperiences, setEducationExperiences] = useState(
+    [
+      {
+        id: uniqid(),
+        school: '',
+        field: '',
+        start: '',
+        end: '',
       },
-      educationExperiences: [
-        {
-          id: uniqid(),
-          school: '',
-          field: '',
-          start: '',
-          end: '',
-        },
-      ],
-      workExperiences: [
-        {
-          id: uniqid(),
-          company: '',
-          position: '',
-          start: '',
-          end: '',
-          description: '',
-        }
-      ],
-    };
-  }
+    ]
+  );
 
-  handleGeneralChange = (event) => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState((prevState) => ({
-      ...prevState,
-      general: {
-        ...prevState.general,
-        [name]: value
+  const [workExperiences, setWorkExperiences] = useState(
+    [
+      {
+        id: uniqid(),
+        company: '',
+        position: '',
+        start: '',
+        end: '',
+        description: '',
       }
-    }));
-  }
+    ]
+  )
 
-  handleWorkChange = (event, key) => {
+  const handleGeneralChange = (event) => {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    const index = this.state.workExperiences.findIndex(experience => experience.id === key);
-     
-    let experience = this.state.workExperiences[index];
+
+    setGeneral({
+      ...general,
+      [name]: value
+    });
+  }
+
+  const handleWorkChange = (event, key) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    const index = workExperiences.findIndex(experience => experience.id === key);
+    
+    let experience = workExperiences[index];
     experience[name] = value;
     
-    let experiences = [...this.state.workExperiences];
+    let experiences = [...workExperiences];
     experiences[index] = experience;
-    
-    this.setState((prevState) => ({
-      ...prevState,
-      workExperiences: experiences,
-    }));
+
+    setWorkExperiences(experiences);
   }
 
-  addWorkExperience = () => {
+  const addWorkExperience = () => {
     const newExperience = {
       id: uniqid(),
       company: '',
@@ -85,42 +82,33 @@ class App extends Component {
       description: '',
     }
 
-    this.setState((prevState) => ({
-      ...prevState,
-      workExperiences: [...prevState.workExperiences, newExperience],
-    }));
+    setWorkExperiences([...workExperiences, newExperience]);
   }
 
-  deleteWorkExperience = (key) => {
-    if (this.state.workExperiences.length === 1) {
+  const deleteWorkExperience = (key) => {
+    if (workExperiences.length === 1) {
       return;
     }
 
-    this.setState((prevState) => ({
-      ...prevState,
-      workExperiences: prevState.workExperiences.filter((experience) => experience.id !== key)
-    }))
+    setWorkExperiences(workExperiences.filter((experience) => experience.id !== key));
   }
 
-  handleEducationChange = (event, key) => {
+  const handleEducationChange = (event, key) => {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    const index = this.state.educationExperiences.findIndex(experience => experience.id === key);
+    const index = educationExperiences.findIndex(experience => experience.id === key);
      
-    let experience = this.state.educationExperiences[index];
+    let experience = educationExperiences[index];
     experience[name] = value;
     
-    let experiences = [...this.state.educationExperiences];
+    let experiences = [...educationExperiences];
     experiences[index] = experience;
-    
-    this.setState((prevState) => ({
-      ...prevState,
-      educationExperiences: experiences,
-    }));
+
+    setEducationExperiences(experiences);
   }
 
-  addEducationExperience = () => {
+  const addEducationExperience = () => {
     const newExperience = {
       id: uniqid(),
       school: '',
@@ -129,61 +117,52 @@ class App extends Component {
       end: '',
     }
 
-    this.setState((prevState) => ({
-      ...prevState,
-      educationExperiences: [...prevState.educationExperiences, newExperience],
-    }));
+    setEducationExperiences([...educationExperiences, newExperience]);
   }
 
-  deleteEducationExperience = (key) => {
-    if (this.state.educationExperiences.length === 1) {
+  const deleteEducationExperience = (key) => {
+     if (educationExperiences.length === 1) {
       return;
     }
 
-    this.setState((prevState) => ({
-      ...prevState,
-      educationExperiences: prevState.educationExperiences.filter((experience) => experience.id !== key)
-    }))
+    setEducationExperiences(educationExperiences.filter((experience) => experience.id !== key));
   }
 
-  render() {
-    const { educationExperiences, workExperiences } = this.state;
-    return (
-      <div className="App">
-        <div className='App-form'>
-          <section className='App-form-section'>
-            <h2>General</h2>
-            <GeneralForm onFormChange={this.handleGeneralChange} />
-          </section>
-          <section className='App-form-section'>
-            <h2>Work Experience</h2>
-            {workExperiences.map((experience) =>
-              <WorkForm key={experience.id} dataKey={experience.id} onFormChange={this.handleWorkChange} onAdd={this.addWorkExperience} onDelete={this.deleteWorkExperience} />
-            )}
-          </section>
-          <section className='App-form-section'>
-            <h2>Education</h2>
-            {educationExperiences.map((experience) =>
-              <EducationForm key={experience.id} dataKey={experience.id} onFormChange={this.handleEducationChange} onAdd={this.addEducationExperience} onDelete={this.deleteEducationExperience} />
-            )}
-          </section>
-        </div>
-        <div className='App-view'>
-          <section className='App-view-section'>
-              <GeneralView {...this.state.general} />
-          </section>
-          <section className='App-view-section'>
-            <p className='App-section-header'>Work Experience</p>
-            {workExperiences.map((experience) => <WorkView key={uniqid()} {...experience} />)}
-          </section>
-          <section className='App-view-section'>
-            <p className='App-section-header'>Education</p>
-            {educationExperiences.map((experience) => <EducationView key={uniqid()} {...experience} />)}
-          </section>
-        </div>
+  return (
+    <div className="App">
+      <div className='App-form'>
+        <section className='App-form-section'>
+          <h2>General</h2>
+          <GeneralForm onFormChange={handleGeneralChange} />
+        </section>
+        <section className='App-form-section'>
+          <h2>Work Experience</h2>
+          {workExperiences.map((experience) =>
+            <WorkForm key={experience.id} dataKey={experience.id} onFormChange={handleWorkChange} onAdd={addWorkExperience} onDelete={deleteWorkExperience} />
+          )}
+        </section>
+        <section className='App-form-section'>
+          <h2>Education</h2>
+          {educationExperiences.map((experience) =>
+            <EducationForm key={experience.id} dataKey={experience.id} onFormChange={handleEducationChange} onAdd={addEducationExperience} onDelete={deleteEducationExperience} />
+          )}
+        </section>
       </div>
-    );
-  }
+      <div className='App-view'>
+        <section className='App-view-section'>
+            <GeneralView {...general} />
+        </section>
+        <section className='App-view-section'>
+          <p className='App-section-header'>Work Experience</p>
+          {workExperiences.map((experience) => <WorkView key={uniqid()} {...experience} />)}
+        </section>
+        <section className='App-view-section'>
+          <p className='App-section-header'>Education</p>
+          {educationExperiences.map((experience) => <EducationView key={uniqid()} {...experience} />)}
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
