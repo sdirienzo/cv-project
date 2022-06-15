3047ecebcc583fbd37ab51cf899431300afa132f
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-15 19:20:06 -0400

    Convert EducationForm from class component to functional component

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/Form.css'
 
-class EducationForm extends Component {
-    handleChange = (event) => {
-        const key = this.props.dataKey
-        this.props.onFormChange(event, key);
+const EducationForm = (props) => {
+    const handleChange = (event) => {
+        const key = props.dataKey
+        props.onFormChange(event, key);
     }
 
-    handleAdd = (event) => {
+    const handleAdd = (event) => {
         event.preventDefault();
-        this.props.onAdd();
+        props.onAdd();
     }
 
-    handleDelete = (event) => {
-        const key = this.props.dataKey;
+    const handleDelete = (event) => {
+        const key = props.dataKey;
         event.preventDefault();
-        this.props.onDelete(key);
+        props.onDelete(key);
     }
 
-    render() {
-        const { dataKey } = this.props;
-        return (
-            <form data-key={dataKey}>
-                <input type='text' name='school' placeholder='School' onChange={this.handleChange} />
-                <input type='text' name='field' placeholder='Field of Study / Course' onChange={this.handleChange} />
-                <input type='text' name='start' placeholder='Start Date' onChange={this.handleChange} />
-                <input type='text' name='end' placeholder='End Date' onChange={this.handleChange} />
-                <div className='btn-row'>
-                    <button className='btn btn-danger' onClick={this.handleDelete}>Delete</button>
-                    <button className='btn btn-success' onClick={this.handleAdd}>Add</button>
-                </div>
-            </form>  
-        );
-    }
-}
+    const { dataKey } = props;
+    return (
+        <form data-key={dataKey}>
+            <input type='text' name='school' placeholder='School' onChange={handleChange} />
+            <input type='text' name='field' placeholder='Field of Study / Course' onChange={handleChange} />
+            <input type='text' name='start' placeholder='Start Date' onChange={handleChange} />
+            <input type='text' name='end' placeholder='End Date' onChange={handleChange} />
+            <div className='btn-row'>
+                <button className='btn btn-danger' onClick={handleDelete}>Delete</button>
+                <button className='btn btn-success' onClick={handleAdd}>Add</button>
+            </div>
+        </form>
+    );
+};
 
 export default EducationForm;
