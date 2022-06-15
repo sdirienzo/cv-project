762689c7f5bdde611724e469fe3ee1c24b6ec266
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-15 19:08:39 -0400

    Convert GeneralForm from class component to functional component

--- src/components/GeneralForm.js
+++ src/components/GeneralForm.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/Form.css'
 
-class GeneralForm extends Component {
-    handleChange = (event) => {
-        this.props.onFormChange(event);
+const GeneralForm = (props) => {
+    const handleChange = (event) => {
+        props.onFormChange(event);
     }
-
-    render() {
-        return (
-            <form>
-                <input type='text' name='name' placeholder='Name' onChange={this.handleChange} />
-                <input type='text' name='title' placeholder='Title' onChange={this.handleChange} />
-                <input type='text' name='phone' placeholder='Phone' onChange={this.handleChange} />
-                <input type='text' name='email' placeholder='Email' onChange={this.handleChange} />
-                <input type='text' name='location' placeholder='Location' onChange={this.handleChange} />
-                <textarea name='summary' placeholder='Summary' onChange={this.handleChange} />
-            </form>  
-        );
-    }
-}
+    
+    return (
+        <form>
+            <input type='text' name='name' placeholder='Name' onChange={handleChange} />
+            <input type='text' name='title' placeholder='Title' onChange={handleChange} />
+            <input type='text' name='phone' placeholder='Phone' onChange={handleChange} />
+            <input type='text' name='email' placeholder='Email' onChange={handleChange} />
+            <input type='text' name='location' placeholder='Location' onChange={handleChange} />
+            <textarea name='summary' placeholder='Summary' onChange={handleChange} />
+        </form>
+    );
+};
 
 export default GeneralForm;
